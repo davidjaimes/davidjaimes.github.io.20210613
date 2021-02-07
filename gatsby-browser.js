339c76911945b6e7db516260e53415de1242f0b8
David Jaimes <david@djaimes.com>
2021-02-06 21:50:15 -0800

    delete all

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-//bootstrap
-import 'bootstrap/dist/css/bootstrap.css';
-
-// prism.js
-require("prismjs/themes/base16-google.light.css")
-
-// katex for LaTeX support 
-require(`katex/dist/katex.min.css`)
--- scripts/wc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-number=$(< .git/logs/HEAD wc -l)
-echo var commits =$number
