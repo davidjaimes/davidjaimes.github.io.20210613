5f4427a2711437ba5e0235548855efb083523c1c
David Jaimes <david@djaimes.com>
2021-03-06 16:23:22 -0800

    add footer icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,17 +3,25 @@ import React from 'react'
 
 // Bootstrap Dependencies
 import {Navbar, Container, Row, Col} from 'react-bootstrap'
-import {} from 'react-icons/fa'
+import {FaGithub, FaLinkedin, FaTwitter, FaInstagram, FaFacebook, FaYoutube} from 'react-icons/fa'
 
 
 export default function Footer() {
 	return (
-		<Navbar expand="lg" variant="dark" bg="secondary" fixed="sticky">
-		  <Container className="justify-content-center mt-5 mb-4">
-				<Row>
+		<Navbar expand="lg" fixed="sticky">
+			<Container className="justify-content-center mt-5 mb-5">
+				<Row className="mt-2 mb-2">
 					<Col style={{maxWidth: "768px"}}>
-						<p className="text-light text-center">&copy; 2021, David Jaimes. All rights reserved.</p>
-						<p className="text-light text-center">Yo <span style={{color: "#f07199"}}>&hearts;</span> Naxiely</p>
+						<p className="text-center">
+							<FaGithub size={30} color={'grey'}/> &nbsp;&nbsp;&nbsp;&nbsp;
+							<FaLinkedin size={30} color={'grey'}/> &nbsp;&nbsp;&nbsp;&nbsp;
+							<FaTwitter size={30} color={'grey'}/> &nbsp;&nbsp;&nbsp;&nbsp;
+							<FaInstagram size={30} color={'grey'}/> &nbsp;&nbsp;&nbsp;&nbsp;
+							<FaFacebook size={30} color={'grey'}/> &nbsp;&nbsp;&nbsp;&nbsp;
+							<FaYoutube size={30} color={'grey'}/> &nbsp;&nbsp;&nbsp;&nbsp;
+						</p>
+						<p className="text-dark text-center" style={{fontSize: "13px"}}>Copyright &copy; 2021 David Jaimes. All rights reserved. | <a href='/About'>About</a> | <a href='/legal'>Terms of Use</a> |</p>
+						<p className="text-dark text-center" style={{fontSize: "15px"}}>Yo <span style={{color: "#f07199"}}>&hearts;</span> Naxiely</p>
 					</Col>
 				</Row>
 			</Container>
