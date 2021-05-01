f718fd1a750e73cceb48dd2224c82b5912db81e8
David Jaimes <david@djaimes.com>
2021-05-01 14:40:39 -0700

    try changing aspect ratio to 120 x 80

--- src/components/card-index.jsx
+++ src/components/card-index.jsx
@@ -39,7 +39,7 @@ export default function CardIndex() {
 							<Badge pill variant="dark">Python</Badge>{' '}
 							<Badge pill variant="dark">AstroPy</Badge>{' '}
 						</Media.Body>
-						<Image src={KOI3570} rounded className="mb-3 border border-secondary" width={120} height={90} alt='KOI-3570'/>
+						<Image src={KOI3570} rounded className="mb-3 border border-secondary" width={120} height={80} alt='KOI-3570'/>
 					</Media>
 					<Card.Text>
 						<b>KOI-3570</b> (aliases: <b>2MASS J19405783+4009273</b>, <b>KIC 5023948</b>, and <b>WISE J194057.82+400927.3</b>) is an eclipsing binary star system. The term KOI means exactly what the name implies – Kepler has declared these to be "objects of interest," not planetary candidates. KOI-3570 is a member of the old open cluster NGC 6819. <b>NGC 6819</b> is an open cluster (commonly known as an "open star cluster") located 7,200 light years away in the Cygnus constellation. It was discovered by Caroline Herschel on 12 May 1784.
@@ -55,7 +55,7 @@ export default function CardIndex() {
 							<Badge pill variant="dark">Astronomy</Badge>{' '}
 							<Badge pill variant="dark">Java</Badge>{' '}
 						</Media.Body>
-						<Image src={JSkyCalc} rounded className="mb-3 border border-secondary" width={120} height={90} alt='JSkyCalc'/>
+						<Image src={JSkyCalc} rounded className="mb-3 border border-secondary" width={120} height={80} alt='JSkyCalc'/>
 					</Media>
 					<Card.Text>
 						<b>JSkycalc</b> expedites the "time-and-the-sky" calculations needed by observational astronomers. To follow the original directions on setting up JSkyCalc on your computer, go to <b>John Thorstensen's</b> website. His directions might be a little outdated (from year 2008) but they still work.
@@ -71,7 +71,7 @@ export default function CardIndex() {
 							<Badge pill variant="dark">Python</Badge>{' '}
 							<Badge pill variant="dark">Matplotlib</Badge>{' '}
 						</Media.Body>
-						<Image src={Exercise1} rounded className="mb-3 border border-secondary" width={120} height={90} alt='Linear Regression'/>
+						<Image src={Exercise1} rounded className="mb-3 border border-secondary" width={120} height={80} alt='Linear Regression'/>
 					</Media>
 					<Card.Text>
 						<b>Linear regression</b> attempts to model the relationship between two variables by fitting a linear equation to observed data. One variable is considered to be an explanatory variable, and the other is considered to be a dependent variable. For example, a modeler might want to relate the weights of individuals to their heights using a <b>linear regression model</b>.
